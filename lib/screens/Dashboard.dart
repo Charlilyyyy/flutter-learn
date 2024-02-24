e9a0773c5fbde908683e65ed41ba6cf02f796b14
Charlilyyyy <mohamadtaufik099@gmail.com>
2024-02-24 11:30:34 +0800

    add logic flutter

--- lib/screens/Dashboard.dart
+++ lib/screens/Dashboard.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class Dashboard extends StatelessWidget{
   const Dashboard({Key? key}) : super(key: key);
@@ -11,22 +12,32 @@ class Dashboard extends StatelessWidget{
           backgroundColor: Colors.blueGrey,
         ),
         body: Center(
-          child: Text.rich(
-            TextSpan(
-              text: 'My', 
-              children: [
-                TextSpan(
-                  text: 'Flutter',
-                  style: TextStyle(fontSize: 50.0, fontWeight: FontWeight.bold)
-                ),
-                TextSpan(
-                  text: 'App',
-                  style: TextStyle(fontSize: 30.0, color: Colors.blue[400])
-                ),
-              ]
-              )
-            ),
+          child: 
+          // Text.rich(
+          //   TextSpan(
+          //     text: 'My', 
+          //     children: [
+          //       TextSpan(
+          //         text: 'Flutter',
+          //         style: TextStyle(fontSize: 50.0, fontWeight: FontWeight.bold)
+          //       ),
+          //       TextSpan(
+          //         text: 'App',
+          //         style: TextStyle(fontSize: 30.0, color: Colors.blue[400])
+          //       ),
+          //     ]
+          //     )
+          //   ),
+          Text('Random value is: ${getNumber()}')
         ),
       );
   }
 }
+
+// flutter fat arrow behave like this
+int getNumber()=> Random().nextInt(100);
+
+//flutter function behave like this
+// int getNumber() {
+//   return Random().nextInt(100);
+// }
